d2b8c163912a7c3e658734c51e79810093886b91
khoa <3minhkhoa23032003@gmail.com>
2023-10-20 12:42:52 +0700

    First

---
+++ bandienthoai/Controllers/TrangChuController.cs
@@ -0,0 +1,34 @@
+﻿using bandienthoai.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bandienthoai.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        QLBANSACHEntities db = new QLBANSACHEntities();
+        // GET: TrangChu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //lay so luong sach 
+        public List<SACH> LaySach(int id)
+        {
+            return db.SACHes.OrderByDescending(s=>s.Ngaycapnhat).Take(id).ToList();
+        }
+        public ActionResult HienThi(){
+        var sl = LaySach(5);
+            return View(sl);
+            
+        }
+        public ActionResult Chitietsp()
+        {
+            return View();
+        }
+
+    }
+}
